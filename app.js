24095eed84dbd0fb7b6b643aa52edc7c3063d4e0
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-04-15 15:46:43 -0300

    CT-27

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const dotenv = require('dotenv');
 const app = express();
+
+dotenv.config();
 
 // Configuração do middleware do express
 app.use(express.json());
