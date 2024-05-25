ce7ce5fe0d1b55da7ae7ed3449712352327561c6
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-05-25 17:47:39 -0300

    Created getUsers() endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Prisma, User } from '@prisma/client';
 
 @Controller('user')
 export class UserController {
@@ -7,6 +8,20 @@ export class UserController {
   constructor(
     private readonly userService: UserService
   ) {}
+
+  @Get('users')
+  async getUsers(): Promise<User[]> {
+    return await this.userService.getUsers();
+  }
+
+  @Post('register')
+  async register(@Body() createAuthDto: Prisma.UserCreateInput): Promise<Prisma.UserCreateInput> {
+    try {
+      return await this.userService.createUser(createAuthDto);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 
   @Post('login')
   async login(@Body('email') email: string, @Body('password') password: string): Promise<string> {
