7f76be12cd77cd5d51b54aed5dbeee1ad4abc3c6
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-05-26 01:21:29 -0300

    Created department CRUD and db seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,25 @@ import configurationConfig from './config/configuration.config';
 import { UserModule } from './modules/user/user.module';
 import { RoleModule } from './modules/role/role.module';
 import { TeamModule } from './modules/team/team.module';
+import { DepartmentModule } from './modules/department/department.module';
+import { SeedModule } from './services/seed/seed.module';
+import { SeedService } from './services/seed/seed.service';
 
 @Module({
   imports: [
     UserModule,
     RoleModule,
     TeamModule,
+    DepartmentModule,
     ConfigModule.forRoot({
       load: [configurationConfig],
       isGlobal: true
     }),
+    SeedModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    SeedService
+  ],
 })
 export class AppModule {}
