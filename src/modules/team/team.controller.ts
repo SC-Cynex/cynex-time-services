49fc711a493402b3cb710427620de45a8ad6b712
humberto-peres <betinhoperes.filho@gmail.com>
2024-05-30 02:18:02 -0300

    Ajustando cotroller de equipe

--- src/modules/team/team.controller.ts
+++ src/modules/team/team.controller.ts
@@ -1,33 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
 import { TeamService } from './team.service';
-import { Prisma } from '@prisma/client';
+import { Prisma, Team } from '@prisma/client';
 
 @Controller('team')
 export class TeamController {
   constructor(private readonly teamService: TeamService) {}
 
   @Post()
-  create(@Body() createTeamDto: Prisma.TeamCreateInput) {
-    return this.teamService.create(createTeamDto);
+  async create(
+    @Body() createTeamDto: Prisma.TeamCreateInput
+  ): Promise<{ status: string; message: string; statusCode: number }> {
+    try {
+      await this.teamService.create(createTeamDto);
+      return {        
+        status: "success",
+        message: "Equipe registrada com sucesso",
+        statusCode: HttpStatus.CREATED,
+      };
+    } catch (error) {
+      return {
+        status: "error",
+        message: "Erro ao registrar a equipe!",
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      };
+    }
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<Team[]> {
     return this.teamService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string): Promise<Team | null> {
     return this.teamService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTeamDto: Prisma.TeamUpdateInput) {
+  async update(@Param('id') id: string, @Body() updateTeamDto: Prisma.TeamUpdateInput): Promise<Team | null> {
     return this.teamService.update(+id, updateTeamDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string): Promise<Team | null> {
     return this.teamService.remove(+id);
   }
 }
